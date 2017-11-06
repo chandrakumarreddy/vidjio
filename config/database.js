5aad128923573d1b475b92e696da2549be3170fc
chandra reddy <chandra reddy>
2017-11-06 17:52:01 +0530

    added

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = { mongoURI: 'mongodb://CHANGEME' }
+    module.exports = { mongoURI: 'mongodb://chandra:chandra@123@ds149495.mlab.com:49495/vidjio' }
 } else {
     module.exports = { mongoURI: 'mongodb://localhost/vidjot-dev' }
 }
